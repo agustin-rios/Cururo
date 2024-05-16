40a3c1e3d58a3444841914f23d08ca8229b8a86b
agustin-rios <arios6@uc.cl>
2024-05-16 15:29:22 -0400

    fix bug with action

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cururo',
-    version='1.1.1',
+    version='1.1.2',
     author='Agustin Rios',
     author_email='arios6@uc.cl',
     description='Review an item using OpenAI.',
