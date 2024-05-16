30d5e196c7106d618a9b497bd93bb6c70823c664
agustin-rios <arios6@uc.cl>
2024-05-16 14:18:41 -0400

    changes that del prints and fix args

--- cururo/cli.py
+++ cururo/cli.py
@@ -3,12 +3,14 @@ import os
 from .reviewer import Reviewer
 from .util.publisher import Publisher
 from .util.handler import ResponseHandler
+from .lib.env_modes import EnvModes
 
 def custom_action(response):
     pass # Custom action to perform on response
 
 DEFAULT_OPENAI_KEY = os.getenv('OPENAI_KEY')
 DEFAULT_ASSISTANT_ID = os.getenv('ASSISTANT_ID')
+DEFAULT_MODE = 'production'
 DEFAULT_ACTION = custom_action
 DEFAULT_PUBLISHER = Publisher()
 
@@ -18,8 +20,11 @@ def main():
     parser.add_argument('--item', type=str, required=True, help='Item to add to the review.')
     parser.add_argument('--openai-key', type=str, default=DEFAULT_OPENAI_KEY, help='OpenAI API key.', required=DEFAULT_OPENAI_KEY is None)
     parser.add_argument('--assistant-id', type=str, default=DEFAULT_ASSISTANT_ID, help='OpenAI assistant ID.', required=DEFAULT_ASSISTANT_ID is None)
-    parser.add_argument('--action', type=callable, default=DEFAULT_ACTION, help='Custom action to perform on response.')
-    parser.add_argument('--publisher', type=Publisher, default=DEFAULT_PUBLISHER, help='Publisher to use for the review.')
+    parser.add_argument('--mode', type=str, default=DEFAULT_MODE, help='Mode to run the review in.', choices=EnvModes.allowed_modes)
+    parser.add_argument('--action', type=str, default=DEFAULT_ACTION, help='Custom action to perform on response.')
+    parser.add_argument('--publisher', type=str, default=DEFAULT_PUBLISHER, help='Publisher to publish results.')
+    # print version
+    parser.add_argument('--version', action='version', version='%(prog)s 1.1.0')
     
     args = parser.parse_args()
 
@@ -30,10 +35,12 @@ def main():
     if isinstance(args.publisher, str):
         # Similar to action, instantiate your publisher or similarly handle it
         args.publisher = eval(args.publisher)  # Ensure security if using eval
+        # print pubihser methods
+        print(dir(args.publisher))
 
 
     handler = ResponseHandler(publisher=args.publisher, additional_actions=[args.action])
-    reviewer = Reviewer(openai_api_key=args.openai_key, assistant_id=args.assistant_id)
+    reviewer = Reviewer(openai_api_key=args.openai_key, assistant_id=args.assistant_id, mode=args.mode)
     reviewer.append_item(args.item)
     reviewer.execute(handler.handle_response)
 
